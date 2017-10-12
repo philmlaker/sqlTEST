8bc96137bab7780012db9f22abc7dd4ba63ea8c7
Philip Mlaker <shuphm@US.Int.SonicHealthcare>
2017-10-12 15:58:58 -0500

    trying to get it to push to the sql database...

--- public/app.js
+++ public/app.js
@@ -9,12 +9,37 @@ $( "#click" ).click(function() {
             success: function(res, req) {
 
                 console.log(res[1].first_name);
-                $("#results").html(res[1].first_name);
+                $("#results").html(res[1].last_name);
 
             }
         });
 });
 
+$("#submit").click(function(event) {
+  event.preventDefault();
+  alert( "Submit" );
+   var value = $("#FirstName").val(); 
+   console.log(value);
+   $.ajax({
+            url: "/firstname",
+            data: "value",
+            type: 'POST'
+          })
+            .done(function(response){
+              console.log("this is from app.js" + response);
+
+            });
+
+
   
+  
+
+});
+
+
+$( "#results" ).on( "click", function() {
+  console.log($( this ).text());
+
+});
 
 });
--- server.js
+++ server.js
@@ -1,25 +1,32 @@
-var express    = require('express'),
-    mysql      = require('mysql'),
+var express = require('express'),
+    mysql = require('mysql'),
     path = require('path');
+
 
 var app = express();
 module.exports - app;
 
+
 var connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : '1234',
-        database : 'sakila'
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'sakila'
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/test", function(req, res) {
+    connection.query('SELECT * FROM actor', function(err, rows) {
+        if (err) { console.log("Error :" + err) } else { res.send(rows); };
     });
-
- app.use(express.static(__dirname + '/public'));
-
-app.get("/test", function(req, res) { 
-	 connection.query('SELECT * FROM actor', function(err, rows){
- 		if (err) {console.log ("Error :" + err)}
-        else { res.send(rows); };
 });
+
+app.post('/firstname', function(req, res) {
+    console.log("This is from server" + req.body);
+    res.json(req.body);
 });
+
 
 
 

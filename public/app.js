cfa4b2bfde62642af0a176b23d5226f67c6c6489
Philip Mlaker <shuphm@US.Int.SonicHealthcare>
2017-10-23 15:18:51 -0500

    added PDF feature, content menu

--- public/app.js
+++ public/app.js
@@ -1,85 +1,125 @@
 $(document).ready(function() {
 
-    $("#clear").click(function(event) {
-        event.preventDefault();
-        alert("You are about to clear the table, get ready!");
-
-        $.ajax({
-                url: "/clear",
-                type: 'DELETE'
-            })
-            .done(function(req, res) {
-
-            });
-    });
+$("#createPDF").click(function(event) {
+    console.log("create PDF b");
+    var column = [];
+    var firstName = [];
+    var lastName = [];
+    var docDefinition = {};
+    var body = [];
 
 
-    $("#submit").click(function(event) {
-        event.preventDefault();
-        alert("Submit");
-        var firstName =  $("#firstName").val();
-        var lastName = $("#lastName").val();
-        var email = $("#email").val();
-        var gender = $("#gender").val();
-        console.log( $("#firstName").val());
-        console.log( $("#lastName").val())
-        console.log( $("#email").val());
-        console.log( $("#gender").val());
-        $.ajax({
+                 
+
+    $.ajax({
+                    url: "/allresults",
+                    type: 'GET',
+                    dataType: 'json',
+                })
+                .done(function(req, res) {
+                    console.log(res);
+                    column.push({text: "First Name"});
+
+                    for (var i = 0; i < req.length; ++i) {
+                        console.log(req[i].first_name);
+                        firstName.push(req[i].first_name);
+                        lastName.push(req[i].last_name);
+                        };
+               
+
+
+
+docDefinition = { content: [
+
+    {
+            style: 'tableExample',
+            table: {
+                body: [["First Name", "Last Name"], [firstName, lastName]]
+            }
+        }]
+};
+   pdfMake.createPdf(docDefinition).open();     
+ });
+});
+
+
+function addPerson (firstName,lastName,email,gender){
+    
+         $.ajax({
                 url: "/add",
-                data: {firstName: firstName, lastName: lastName, email: email, gender: gender},
+                data: { firstName: firstName, lastName: lastName, email: email, gender: gender },
                 type: 'POST',
                 dataType: 'json'
             })
             .done(function(req, res) {
                 console.log("complete, from app.js");
-                console.log(req);
-                console.log(res);
-            });
-    });
-
-var getResults = function(){
-    $("#click-all-results").click(function() {
-        alert("All Results Coming Up!");
-        $.ajax({
-                url: "/allresults",
-                type: 'GET',
-                dataType: 'json',
-            })
-            .done(function(req, res) {
-                console.log(req);
-                console.log(req.length);
-                $("#all-results").empty();
-
-
-
-
-                var myTableArray = [];
-                myTableArray.push("<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>E-mail</th><th>Gender</th><tr>");
-
-                for (var i = 0; i < req.length; ++i) {
-                    myTableArray.push(
-
-                        "<tr><td>" + req[i].id + "</td>" +
-                        "<td>" + req[i].first_name + "</td>" +
-                        "<td>" + req[i].last_name + "</td>" +
-                        "<td>" + req[i].email + "</td>" +
-                        "<td>" + req[i].gender + "</td>" +
-                        "<td><button id=" + "delete" + ">DELETE</button></td></tr>"
-                    );
-
-                };
-
-
-                $("#all-results").append(myTableArray);
-                $("#totalUsers").html(req.length);
 
             });
-    });
 
+}
+
+
+    $("#submit").click(function(event) {
+        event.preventDefault();
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        var email = $("#email").val();
+        var gender = $("#gender").val();
+
+
+    function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 };
+    
+    firstName = capitalizeFirstLetter(firstName);
+    lastName = capitalizeFirstLetter(lastName);
 
-getResults();
+        formValidation(firstName,lastName,email,gender);
+
+       
+    });
+
+
+        $("#click-all-results").click(function() {
+            alert("All Results Coming Up!");
+            $.ajax({
+                    url: "/allresults",
+                    type: 'GET',
+                    dataType: 'json',
+                })
+                .done(function(req, res) {
+                    console.log(req);
+                    console.log(req.length);
+                    $("#all-results").empty();
+
+
+
+
+                    var myTableArray = [];
+                    myTableArray.push("<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>E-mail</th><th>Gender</th><tr>");
+
+                    for (var i = 0; i < req.length; ++i) {
+                        myTableArray.push(
+
+                            "<tr><td>" + req[i].id + "</td>" +
+                            "<td>" + req[i].first_name + "</td>" +
+                            "<td>" + req[i].last_name + "</td>" +
+                            "<td>" + req[i].email + "</td>" +
+                            "<td>" + req[i].gender + "</td>" +
+                            "<td><button id=" + "delete" + ">DELETE</button></td></tr>"
+                        );
+
+                    };
+
+
+                    $("#all-results").append(myTableArray);
+                    $("#totalUsers").html(req.length);
+
+                });
+        });
+
+
+
 
     $('body').on('click', '#delete', function() {
 
@@ -98,64 +138,119 @@ getResults();
 
 
         $.ajax({
-        url: url_id,
-        type: 'DELETE',
-        dataType: 'json',
-    })
-    .done(function(req, res) {
-
- console.log("complete, from app.js");
-
-
-                             $("#all-results").empty();
-                         
-
-                                                alert("All Results Coming Up!");
-        $.ajax({
-                url: "/allresults",
-                type: 'GET',
+                url: url_id,
+                type: 'DELETE',
                 dataType: 'json',
             })
             .done(function(req, res) {
-                console.log(req);
-                console.log(req.length);
+
+                console.log("complete, from app.js");
+
+
                 $("#all-results").empty();
 
 
+                alert("All Results Coming Up!");
+                $.ajax({
+                        url: "/allresults",
+                        type: 'GET',
+                        dataType: 'json',
+                    })
+                    .done(function(req, res) {
+                        console.log(req);
+                        console.log(req.length);
+                        $("#all-results").empty();
 
 
-                var myTableArray = [];
-                myTableArray.push("<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>E-mail</th><th>Gender</th><tr>");
-
-                for (var i = 0; i < req.length; ++i) {
-                    myTableArray.push(
-
-                        "<tr><td>" + req[i].id + "</td>" +
-                        "<td>" + req[i].first_name + "</td>" +
-                        "<td>" + req[i].last_name + "</td>" +
-                        "<td>" + req[i].email + "</td>" +
-                        "<td>" + req[i].gender + "</td>" +
-                        "<td><button id=" + "delete" + ">DELETE</button></td></tr>"
-                    );
-
-                };
 
 
-                $("#all-results").append(myTableArray);
-                $("#totalUsers").html(req.length);
+                        var myTableArray = [];
+                        myTableArray.push("<tr><th>Id</th><th>First Name</th><th>Last Name</th><th>E-mail</th><th>Gender</th><tr>");
+
+                        for (var i = 0; i < req.length; ++i) {
+                            myTableArray.push(
+
+                                "<tr><td>" + req[i].id + "</td>" +
+                                "<td>" + req[i].first_name + "</td>" +
+                                "<td>" + req[i].last_name + "</td>" +
+                                "<td>" + req[i].email + "</td>" +
+                                "<td>" + req[i].gender + "</td>" +
+                                "<td><button id=" + "delete" + ">DELETE</button></td></tr>"
+                            );
+
+                        };
+
+
+                        $("#all-results").append(myTableArray);
+                        $("#totalUsers").html(req.length);
+
+                    });
+
+
+
+
+
+
+
+
 
             });
-
-
-
-
-
-
-
-
-
     });
-    });
+
+
+
+
+
+
+    function formValidation(firstname, lastname, email, gender) {
+
+
+
+
+
+        function allLetter(firstname,lastname) {
+            var letters = /^[A-Za-z]+$/;
+            if (firstname.match(letters) && lastname.match(letters)) {
+                return true;
+            } else if(!(lastname.match(letters)) && !(firstname.match(letters))) {
+                alert('First and Last Name must have alphabet characters only');
+                $("#lastName, #firstName").focus();
+                return false;
+            }else if( !(firstname.match(letters)) ) {
+                alert('First Name must have alphabet characters only');
+                $("#firstName").focus();
+                return false;
+            } else if( !(lastname.match(letters))) {
+                alert('Last Name must have alphabet characters only');
+                $("#lastName").focus();
+                return false;
+            } 
+        };
+
+       
+        function ValidateEmail(email) {
+            var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            if (email.match(mailformat)) {
+                return true;
+            } else {
+                alert("You have entered an invalid email address!");
+                $("#email").focus();
+                return false;
+            }
+        };
+
+        if((allLetter(firstname,lastname) == true) && (ValidateEmail(email)) == true){
+            alert("Congrats");
+            addPerson(firstname, lastname, email, gender);
+        } else {alert("bad"); return false;}
+
+
+
+    };
+
+
+
+
 
 
 });
